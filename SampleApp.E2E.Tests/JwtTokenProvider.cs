c5c9b73bcbf2f55d59d6d2f25ba11c925f8259b8
Frode Hus <frode.hus@easee.com>
2023-08-08 14:21:53 +0200

    updated packages so we enforce proper key length for signing credentials

--- SampleApp.E2E.Tests/JwtTokenProvider.cs
+++ SampleApp.E2E.Tests/JwtTokenProvider.cs
@@ -1,4 +1,5 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using Microsoft.IdentityModel.Tokens;
 
 namespace SampleApp.E2E.Tests;
@@ -7,7 +8,9 @@ public static class JwtTokenProvider
 {
     public static string Issuer { get; } = "Sample_Auth_Server";
     public static SecurityKey SecurityKey { get; } =
-        new SymmetricSecurityKey(Guid.NewGuid().ToByteArray());
+        new SymmetricSecurityKey(
+            Encoding.ASCII.GetBytes("This_is_a_super_secure_key_and_you_know_it")
+        );
     public static SigningCredentials SigningCredentials { get; } =
         new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256);
     internal static readonly JwtSecurityTokenHandler JwtSecurityTokenHandler = new();
